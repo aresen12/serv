01e04a8e2e97c7ba111edcde27f2ee71116260c8
aresen12 <Ares.Ares.Ars@yandex.ru>
2024-02-07 17:03:28 +0300

    mars

---
+++ Изображение Марса/main.py
@@ -0,0 +1,78 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def mass():
+    return '''<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <h1>Миссия Колонизация Марса</h1>
+</head>
+<body>
+
+</body>
+</html>'''
+
+
+@app.route('/index')
+def dev():
+    return '''<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <h1>И на Марсе будут яблони цвести!</h1>
+</head>
+<body>
+
+</body>
+</html>'''
+
+
+@app.route('/promotion')
+def rek():
+    return '''<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <h1>Человечество вырастает из детства.</h1>
+
+<h1>Человечеству мала одна планета.</h1>
+
+<h1>Мы сделаем обитаемыми безжизненные пока планеты.</h1>
+
+<h1>И начнем с Марса!</h1>
+
+<h1>Присоединяйся!</h1>
+</head>
+<body>
+
+</body>
+</html>'''
+
+
+@app.route('/image_mars')
+def mars():
+    return '''<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Привет, Марс!</title>
+    <h1>Жди нас, Марс!</h1>
+    <img src="mars.jpg"
+           alt="здесь должна была быть картинка, но не нашлась">
+    <br>
+    Вот она какая, красная планета
+</head>
+<body>
+</body>
+</html>'''
+
+
+if __name__ == "__main__":
+    app.run(port="8000", host="127.0.0.1")
