fe0a789eee89f4cd788eebe6733ce6b2606b3732
aresen12 <Ares.Ares.Ars@yandex.ru>
2024-04-04 17:50:40 +0300

    style.css, img, boostrap

--- Реклама с картинкой/main.py
+++ Реклама с картинкой/main.py
@@ -34,29 +34,6 @@ def dev():
 
 
 @app.route('/promotion')
-def promotion():
-    return '''<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-    <h1>Человечество вырастает из детства.</h1>
-
-<h1>Человечеству мала одна планета.</h1>
-
-<h1>Мы сделаем обитаемыми безжизненные пока планеты.</h1>
-
-<h1>И начнем с Марса!</h1>
-
-<h1>Присоединяйся!</h1>
-</head>
-<body>
-
-</body>
-</html>'''
-
-
-@app.route('/promotion_image')
 def rek():
     return '''<!DOCTYPE html>
 <html lang="en">
@@ -81,34 +58,27 @@ def rek():
 
 @app.route('/image_mars')
 def mars():
-    return f'''<!doctype html>
+    return f'''<!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Привет, Яндекс!</title>
+    <meta charset="UTF-8">
+    <title>Привет, Марс!</title>
+    <h1>Жди нас, Марс!</h1>
+    <img src="{url_for('static', filename='img/mars.jpg')}" 
+           alt="здесь должна была быть картинка, но не нашлась">
+    <br>
+    Вот она какая, красная планета
 </head>
 <body>
-<h1 style="color: red;">Жди нас, Марс!</h1>
-<img src="{url_for('static', filename='img/mars.jpg')}" 
-           alt="здесь должна была быть картинка, но не нашлась">
-<div class="rec1" role="alert" style="background: #d6d8d9;margin-top: 8px;width:450px;height:30px; font-size:20px">
-    Человечество вырастает из детства.
-</div>
-<div class="rect2" role="alert" style="background: #d4edda;margin-top: 8px;width:450px;height:30px;color: #155724;font-size:20px">
-    Человечеству мала одна планета.
-</div>
-<div class="rect3" role="alert" style="background: #e2e3e5;margin-top: 8px;width:450px;height:45px;color: #383d41;font-size:20px">
-Мы сделаем обитаемыми безжизненные пока планеты.
-</div>
-<div class="rect4" role="alert" style="background: #fff3cd;margin-top: 8px;width:450px;height:30px;color: #856404;font-size:20px">
-И начнем с Марса!
-</div>
-<div class="rect5" role="alert" style="background: #f8d7da;margin-top: 8px;width:450px;height:30px;color: #721c24;font-size:20px">
-Присоединяйся!
-</div>
 </body>
 </html>'''
+
+@app.route("/promotion_image")
+def promotion_image():
+    file = open("promotion_image.html", encoding='utf-8', mode='r')
+    data = file.read()
+    file.close()
+    return data
 
 
 if __name__ == "__main__":
